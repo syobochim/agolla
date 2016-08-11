87f05c5fdf7607c5421946d31bbcf68ca7df185f
Taichi Uragami <backpaper0@gmail.com>
2016-08-11 16:15:17 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Agolla
+
+## How to run
+
+Execute commands as following:
+
+```sh
+gradlew bootRun
+```
+
+And browse `localhost:8080` .
+
+## License
+
+Licensed under [The MIT License](https://opensource.org/licenses/MIT)
+
---
+++ src/main/java/agolla/Agolla.java
@@ -0,0 +1,11 @@
+package agolla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Agolla {
+    public static void main(String[] args) {
+        SpringApplication.run(Agolla.class, args);
+    }
+}
